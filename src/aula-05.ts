34471eb69aaa48430d5af2ebb18d330d9ce1e57d
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-31 22:07:39 -0300

    feat: aula05

---
+++ src/aula-05.ts
@@ -0,0 +1,31 @@
+interface Estado {
+    nome: string;
+}
+
+interface Cidade {
+    nome: string;
+    estado: Estado;
+}
+
+interface Endereco {
+    rua: string;
+    bairro: string;
+    cidade: Cidade;
+    complemento?: string;
+}
+
+const endereco: Endereco = {
+    bairro: "Pimentas",
+    rua: "Broadway",
+    complemento: "apt401",
+    cidade: {
+        nome: "Guarulhos",
+        estado: {
+            nome: "São Paulo",
+        },
+    },
+}
+
+
+console.log(endereco.complemento?.substring(3) || "Deu erro");
+console.log(endereco);
