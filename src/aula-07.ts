0a10f0c168d8509a472b4a14b02004b1fade4810
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-31 23:12:15 -0300

    feat: aula07

---
+++ src/aula-07.ts
@@ -0,0 +1,19 @@
+const funcaoCallBack = (valor: number): void => {
+  console.log("Deu bom" + valor);
+};
+
+const funcaoTeste2 = (
+    valor1: number, 
+    valor2: number, 
+    callback: (n: number) => void
+): string => {
+    if (valor1 > valor2) {
+        callback(valor1);
+    }
+    return "";
+};
+
+funcaoTeste2(23, 32, funcaoCallBack);
+funcaoTeste2(43, 32, (x: number) => {
+    console.log("outra funcao" + x);
+});
