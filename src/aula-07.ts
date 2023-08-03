628765278d3e6fab0675904b61f65a5f1d2a78af
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-08-02 22:29:49 -0300

    feat: aula09

--- src/aula-07.ts
+++ src/aula-07.ts
@@ -1,16 +1,15 @@
 const funcaoCallBack = (valor: number): void => {
-    console.log("Deu bom" + valor);
-  };
-  
-  const funcaoTeste2 = (valor1: number, valor2: number, callback: (n: number) => void): string => {
-    if (valor1 > valor2) {
-      callback(valor1);
-    }
-    return "";
-  };
-  
-  funcaoTeste2(23, 32, funcaoCallBack);
-  funcaoTeste2(43, 32, (x: number) => {
-    console.log("outra funcao" + x);
-  });
-  
+  console.log("Deu bom" + valor);
+};
+
+const funcaoTeste2 = (valor1: number, valor2: number, callback: (n: number) => void): string => {
+  if (valor1 > valor2) {
+    callback(valor1);
+  }
+  return "";
+};
+
+funcaoTeste2(23, 32, funcaoCallBack);
+funcaoTeste2(43, 32, (x: number) => {
+  console.log("outra funcao" + x);
+});
